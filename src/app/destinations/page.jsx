0ffb63b4eadf3fc807a741238e194c65b8627da8
Israt Jahan <eraisrat022@gmail.com>
2026-05-10 01:12:01 +0600

    Connect DB and add destincation page and create destination page

---
+++ src/app/destinations/page.jsx
@@ -0,0 +1,19 @@
+import DestinationCard from "@/components/DestinationCard";
+
+const Destinations = async() => {
+    const res = await fetch('http://localhost:5000/destination');
+    const destinations = await res.json();
+    return (
+        <div className='container mx-auto'>
+            <h1 className='text-2xl font-bold text-center my-2'>Total Destinations</h1>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-5">
+                {
+                    destinations.map(destination => <DestinationCard key={destination._id} destination={destination} />)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Destinations;
